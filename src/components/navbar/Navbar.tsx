5d8c827b0fbad34dce1dc463a90624cd1157b064
otonebieridze <otonebieridze123@gmail.com>
2023-06-11 15:20:49 +0400

    Add navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+import styles from "./Navbar.module.css";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
+function Navbar() {
+  return (
+    <div className={styles.container}>
+      <Link to="/"><img src={logo} alt="logo" /></Link>
+      
+      <ul>
+        <Link className={styles.link} to="/"><li>home</li></Link>
+        <Link className={styles.link} to="/headphones"><li>headphones</li></Link>
+        <Link className={styles.link} to="/speakers"><li>speakers</li></Link>
+        <Link className={styles.link} to="/earphones"><li>earphones</li></Link>
+      </ul>
+
+      <FontAwesomeIcon className={styles["cart-icon"]} icon={faCartShopping} />
+    </div>
+  )
+}
+
+export default Navbar
